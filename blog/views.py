70fd5b056dadbbab42083d25f44ef9746c2a4cea
tiantian5374 <tiantian5374@163.com>
2021-09-19 20:46:28 +0800

    markdown文章自动生成目录

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.text import slugify
+from markdown.extensions.toc import TocExtension
 import markdown
+import re
 
 from .models import Post
 
@@ -13,9 +16,14 @@ def index(request):
 def detail(request, pk):
     # 详情页函数
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body, extensions=[
+    md = markdown.Markdown(extensions=[
         'markdown.extensions.extra',
         'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
+        # 记得在顶部引入 TocExtension 和 slugify
+        TocExtension(slugify=slugify),
     ])
+    post.body = md.convert(post.body)
+
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
